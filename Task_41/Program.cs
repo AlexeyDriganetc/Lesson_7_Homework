76486c64d5cd3bbb161c8d04ce1f81c350ff9718
Alexey Driganets <driganetc@mail.ru>
2023-04-01 10:47:45 +0300

    задача 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.WriteLine("Введите числа: ");
+int[] size = Console.ReadLine()!.Split().Select(i => int.Parse(i)).ToArray();
+int pozitiv = 0;
+for (int m = 0; m < size.Length; m++)
+{
+    if (size[m] > 0)
+    {
+        pozitiv++;
+    }
+}
+Console.WriteLine($"{pozitiv}");
